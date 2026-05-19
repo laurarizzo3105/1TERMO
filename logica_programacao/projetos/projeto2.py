427495e82c42134b76e1ea2d06ad7d7d28981424
Laura Rizzo <lauraguilherme11@gmail.com>
2026-05-19 11:28:48 -0300

    atualização 19.05.26

---
+++ logica_programacao/projetos/projeto2.py
@@ -0,0 +1,5 @@
+# Sistema de Elevador de Prédio
+# O prédio possui 10 andares, sendo o térreo o andar 0. O elevador pode se mover para cima ou para baixo, e tem a capacidade de transportar até 5 pessoas.
+# O elevador começa no andar 0 e pode ser chamado por qualquer pessoa em qualquer andar.
+# O elevador deve se mover para o andar onde a pessoa chamou, e depois para o andar destino da pessoa.
+# O elevador deve exibir mensagens indicando o andar atual, o número de pessoas no elevador, e as ações realizadas (subindo, descendo, parando). O programa deve continuar rodando até que o usuário decida encerrar.
